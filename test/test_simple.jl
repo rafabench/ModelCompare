a4f15bb150614d23bf94b49289ff931c8d7564be
rafabench_psr <rafabench@psr-inc.com>
2020-02-07 15:29:37 -0300

    Test attribute get_bounds from compare_models function

--- test/test_simple.jl
+++ test/test_simple.jl
@@ -1,4 +1,7 @@
 using ModelComparator
 
+@time compare_models(file1 = "models/model1.mps",file2 = "models/model2.mps", get_bounds = false, outfile = "models/compare_no_bounds.txt", tol = 0)
+@time compare_models(file1 = "models/modelbiglp1.mps",file2 = "models/modelbiglp2.mps", get_bounds = false, outfile = "models/comparebiglp_no_bounds.txt", tol = 0)
+
 @time compare_models(file1 = "models/model1.mps",file2 = "models/model2.mps", get_bounds = true, outfile = "models/compare.txt", tol = 0)
 @time compare_models(file1 = "models/modelbiglp1.mps",file2 = "models/modelbiglp2.mps", get_bounds = true, outfile = "models/comparebiglp.txt", tol = 0)
