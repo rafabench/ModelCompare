cf9102e36d5f31a392e36da9089ad50a7d53b1f3
rafabench_psr <rafabench@psr-inc.com>
2020-02-07 15:26:10 -0300

    Add more tolerance tests

--- test/test_tolerance.jl
+++ test/test_tolerance.jl
@@ -1,4 +1,7 @@
 using ModelComparator
 
-@time compare_models(file1 = "models/model_tol_1.mps",file2 = "models/model_tol_2.mps", get_bounds = true, outfile = "models/compare_tol.txt", tol = 1e-3)
-@time compare_models(file1 = "models/modelbiglp_tol_1.mps",file2 = "models/modelbiglp_tol_2.mps", get_bounds = true, outfile = "models/comparebiglp_tol.txt", tol = 1e-3)
+@time compare_models(file1 = "models/model_tol_1.mps",file2 = "models/model_tol_2.mps", get_bounds = true, outfile = "models/compare_tol_e-3.txt", tol = 1e-3)
+@time compare_models(file1 = "models/modelbiglp_tol_1.mps",file2 = "models/modelbiglp_tol_2.mps", get_bounds = true, outfile = "models/comparebiglp_tol_e-3.txt", tol = 1e-3)
+
+@time compare_models(file1 = "models/model_tol_1.mps",file2 = "models/model_tol_2.mps", get_bounds = true, outfile = "models/compare_tol_e-4.txt", tol = 1e-4)
+@time compare_models(file1 = "models/modelbiglp_tol_1.mps",file2 = "models/modelbiglp_tol_2.mps", get_bounds = true, outfile = "models/comparebiglp_tol_e-4.txt", tol = 1e-4)
