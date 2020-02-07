15aa700f4fe3310b317319fa98002b5c0c485aa8
rafabench_psr <rafabench@psr-inc.com>
2020-02-07 14:50:26 -0300

    Create a new folder for models

--- test/test_simple.jl
+++ test/test_simple.jl
@@ -1,4 +1,4 @@
 using ModelComparator
 
-@time compare_models(file1 = "model1.mps",file2 = "model2.mps", get_bounds = true, outfile = "compare.txt", tol = 0)
-@time compare_models(file1 = "modelbiglp1.mps",file2 = "modelbiglp2.mps", get_bounds = true, outfile = "comparebiglp.txt", tol = 0)
+@time compare_models(file1 = "models/model1.mps",file2 = "models/model2.mps", get_bounds = true, outfile = "models/compare.txt", tol = 0)
+@time compare_models(file1 = "models/modelbiglp1.mps",file2 = "models/modelbiglp2.mps", get_bounds = true, outfile = "models/comparebiglp.txt", tol = 0)
--- test/test_tolerance.jl
+++ test/test_tolerance.jl
@@ -1,4 +1,4 @@
 using ModelComparator
 
-@time compare_models(file1 = "model_tol_1.mps",file2 = "model_tol_2.mps", get_bounds = true, outfile = "compare_tol.txt", tol = 1e-3)
-@time compare_models(file1 = "modelbiglp_tol_1.mps",file2 = "modelbiglp_tol_2.mps", get_bounds = true, outfile = "comparebiglp_tol.txt", tol = 1e-3)
+@time compare_models(file1 = "models/model_tol_1.mps",file2 = "models/model_tol_2.mps", get_bounds = true, outfile = "models/compare_tol.txt", tol = 1e-3)
+@time compare_models(file1 = "models/modelbiglp_tol_1.mps",file2 = "models/modelbiglp_tol_2.mps", get_bounds = true, outfile = "models/comparebiglp_tol.txt", tol = 1e-3)
