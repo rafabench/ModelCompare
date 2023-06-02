e5bd491e29138b5d82210712b182fe248a048696
rafabench <rafa.bench@gmail.com>
2023-06-02 16:42:28 -0300

    update tests

--- test/test_simple.jl
+++ test/test_simple.jl
@@ -1,29 +1,29 @@
 using ModelCompare
 
 @time compare_models(
-    file1 = "test/models/model1.mps",
-    file2 = "test/models/model2.mps",
+    "test/models/model1.mps",
+    "test/models/model2.mps",
     outfile = "test/models/compare_mps.txt", 
-    tol = 0
+    tol = 0.0
 )
 
 @time compare_models(
-    file1 = "test/models/model1.lp",
-    file2 = "test/models/model2.lp",
+    "test/models/model1.lp",
+    "test/models/model2.lp",
     outfile = "test/models/compare_lp.txt",
-    tol = 0
+    tol = 0.0
 )
 
 @time compare_models(
-    file1 = "test/models/modelbiglp1.mps",
-    file2 = "test/models/modelbiglp2.mps",
+    "test/models/modelbiglp1.mps",
+    "test/models/modelbiglp2.mps",
     outfile = "test/models/comparebiglp_mps.txt", 
-    tol = 0
+    tol = 0.0
 )
 
 @time compare_models(
-    file1 = "test/models/modelbiglp1.lp",
-    file2 = "test/models/modelbiglp2.lp",
+    "test/models/modelbiglp1.lp",
+    "test/models/modelbiglp2.lp",
     outfile = "test/models/comparebiglp_lp.txt", 
-    tol = 0
+    tol = 0.0
 )
